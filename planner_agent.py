81043e182cf579ca0e7b5024117fafa2f9cf9ed4
ShubhamZoro <89676909+ShubhamZoro@users.noreply.github.com>
2026-02-11 15:02:24 +0530

    Increasing the number of searches

--- planner_agent.py
+++ planner_agent.py
@@ -6,7 +6,7 @@ load_dotenv(override=True)
 
 # See note above about cost of WebSearchTool
 
-HOW_MANY_SEARCHES = 3
+HOW_MANY_SEARCHES = 10
 
 INSTRUCTIONS = f"You are a helpful research assistant. Given a query, come up with a set of web searches \
 to perform to best answer the query. Output {HOW_MANY_SEARCHES} terms to query for."
